a1b27d92088f66a3bff2a3a8ec206fc0e424fb16
Felipe Jung <felipemattoseu@gmail.com>
2020-04-15 17:23:02 -0400

    Continuing project

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '1302313273539504d5bd7c4cbdb71593',
+    expiresIn: '1d',
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,35 @@
-import express from 'express';
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(' 🔴 ⚠ 🔴', err);
+
+  return response.status(500).json({
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('✔ Server started on port 3333');
